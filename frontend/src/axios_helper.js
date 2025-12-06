a12271aee36d7b3f3b5f9f46983918edff073f76
Bonyhai Alexandru Ștefan <alex.s.bony.02@gmail.com>
2025-12-06 16:56:42 +0200

    Updated frontend and traefik

--- frontend/src/axios_helper.js
+++ frontend/src/axios_helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8084';
+axios.defaults.baseURL = 'http://localhost';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
+        // Permite accesul din afara containerului
         host: true,
+        // Portul intern din container
         port: 5173,
-        strictPort: true,
         watch: {
             usePolling: true,
+            interval: 1000,
+            binaryInterval: 300,
+            ignored: ['**/node_modules/**', '**/.git/**', '**/dist/**', '**/vite.config.js']
         },
+        // --- CONFIGURARE HMR OPTIMIZATĂ PENTRU TRAEFIK ---
         hmr: {
-            clientPort: 5173,
-            host: "localhost"
+            // Forțăm protocolul WebSocket standard (nu wss încă, că nu avem HTTPS)
+            protocol: 'ws',
+            // Îi spunem explicit browserului să se conecteze la localhost
+            host: 'localhost',
+            // Portul pe care îl vede browserul (Traefik)
+            clientPort: 80
         }
-    }
+    },
 })
